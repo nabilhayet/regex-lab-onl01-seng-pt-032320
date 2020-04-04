b921f5f21393d6b72cbb9767e15a855b0a8cc61a
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-04 23:38:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ end
 end 
 
 def valid_phone_number?(phone)
- if phone.match(/(\A[2-9]\d{2}+\d{3}+\d{4})| (\A[2-9]\d{2}\s+\d{3}\s+\d{4})/)
+ if phone.match(/(\A[2-9]\d{2}+\d{3}+\d{4})| (\A[2-9]\d{2}\s+\d{3}\s+\d{4})| /)
    return true
   else
     return false
